936ac672648d65a29d812f08c1bd9dfbc6327ec0
priya <ST10072500@vcconnect.edu.za>
2024-04-14 21:04:51 +0200

    Declaring the Getters and Setters to minipulate the Name, Quantity and Unit of Measurement

--- Ingredients.cs
+++ Ingredients.cs
@@ -9,10 +9,17 @@ namespace PROG_6221_Part_1_ST10072500_Priya
 {
     class Ingredients
     {
-        
+        //Gets and Sets the Ingredient's Name
+        public string Name { get; set; }
+
+        //Gets and Sets the Ingredient's Quantity
+        public double Quantity { get; set; }
+
+        //Gets and Sets the Ingredient's Unit of Measurement 
+        public string Unit { get; set; }
 
 
-        
+
 
 
     }
--- Recipe.cs
+++ Recipe.cs
@@ -30,5 +30,7 @@ namespace PROG_6221_Part_1_ST10072500_Priya
 
 
 
+
+
     }
 }
