5b2197f6a53aac443dd4071e7865ed75b4f046cf
priya <ST10072500@vcconnect.edu.za>
2024-04-14 19:42:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG_6221_Part_1_ST10072500_Priya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
