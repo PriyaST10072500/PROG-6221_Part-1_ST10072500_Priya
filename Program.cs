f819ea9af38616843bb3bb8a615e9f3165c43ce6
priya <ST10072500@vcconnect.edu.za>
2024-04-14 22:00:27 +0200

    Creating Menu for the user to interact with and calling methods from the Recipe Class

--- Program.cs
+++ Program.cs
@@ -17,8 +17,54 @@
             Console.WriteLine("------------------------------------------");
 
 
+            Recipe recipe = new Recipe();
 
+            //Displays the Menu
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("\n Menu:");
+                Console.WriteLine(" 1. Enter Recipe Details");
+                Console.WriteLine(" 2. Display Recipe");
+                Console.WriteLine(" 3. Scale Recipe");
+                Console.WriteLine(" 4. Reset Quantities");
+                Console.WriteLine(" 5. Clear Data");
+                Console.WriteLine(" 6. Exit");
+                Console.WriteLine("------------------------------------------");
+                Console.Write(" Select an option: ");
+                string option = Console.ReadLine();
+                Console.WriteLine("------------------------------------------");
 
+                switch (option)
+                {
+                    case "1":
+                        recipe.InputRecipeDetails();
+                        break;
+                    case "2":
+                        recipe.RecipeToBeDisplayed();
+                        break;
+                    case "3":
+                        recipe.RecipeToBeScaled();
+                        break;
+                    case "4":
+                        recipe.QuantitiesToBeReset();
+                        break;
+                    case "5":
+                        recipe.DataToBeCleared();
+                        break;
+                    case "6":
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine(" Invalid option! Please select again.");
+                        break;
+                }
+            }
+
+            Console.WriteLine(" Exiting The Recipe Application!");
         }
     }
+
 }
+    
+
